8f4cdf0ae59dda3f3db46e9c723e4b9fe60a7f3c
menouaw <menoua.khttn@gmail.com>
2025-09-25 21:20:19 +0200

    refactor: architecture

---
+++ include/Game/map.h
@@ -0,0 +1,8 @@
+//
+// Created by menou on 25/09/2025.
+//
+
+#ifndef BATTLESHIP_MAP_H
+#define BATTLESHIP_MAP_H
+
+#endif //BATTLESHIP_MAP_H
---
+++ src/Game/map.c
@@ -0,0 +1,5 @@
+//
+// Created by menou on 25/09/2025.
+//
+
+#include "../../include/Game/map.h"
